5f01d2b9984131bd65c899586c329c9bbd50618c
Mahdi Hasan Shuvo <98658558+Shuvo-BBHH@users.noreply.github.com>
2023-04-14 00:47:40 +0600

    Update Shuvo.py

--- Shuvo.py
+++ Shuvo.py
@@ -5,6 +5,6 @@ from platform import uname
 arch=uname().machine.lower()
 if 'aarch' in arch:
     print(' \n\033[1;37m Congratulations! Your Device Support This Tools');time.sleep(2)
-    import mahdii
+    import mahdii.server()
 else:
-   import shuvo
+   import mahdi
