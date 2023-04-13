d07820c10c94d4ec00dec790ee5005eaa9a3c0bb
Mahdi Hasan Shuvo <98658558+Shuvo-BBHH@users.noreply.github.com>
2023-04-14 00:30:48 +0600

    Update ma.py

--- ma.py
+++ ma.py
@@ -1,10 +1,4 @@
 import os,time
 os.system('clear')
-os.system('xdg-open https://facebook.com/groups/351076900316263/')
-from platform import uname
-arch=uname().machine.lower()
-if 'aarch' in arch:
-    print(' \n\033[1;37m Congratulations! Your Device Support This Tools');time.sleep(2)
-    import mahdii
-else:
-    print(' \033[1;31m Sorry system or device not support this tools');exit()
+os.system("git pull")
+import mahdii
