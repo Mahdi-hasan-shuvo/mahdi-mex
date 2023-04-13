749d56266ce22c6b57b0c2606b551a9b0459541c
Mahdi Hasan Shuvo <98658558+Shuvo-BBHH@users.noreply.github.com>
2023-04-14 00:36:15 +0600

    Update ma.py

--- ma.py
+++ ma.py
@@ -1,5 +1,9 @@
 import os,time
 os.system('clear')
-os.system("git pull")
-import mahdii
-mahdii.server()
+from platform import uname
+arch=uname().machine.lower()
+if 'aarch' in arch:
+    print(' \n\033[1;37m Congratulations! Your Device Support This Tools');time.sleep(2)
+    import mahdii
+else:
+   import mahdi
